bd66c7f2de2a8e7c774eaae863ce545e3d71ae0f
Jonathan881005 <a1813203@gmail.com>
2021-04-10 15:48:21 +0800

    Decide on buying and selling operations

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # AutoTrading
-2021DSAI-HW2
+## Usage
+`python trader.py --training training.csv --testing testing.csv --output output.csv`
+## Data analysis
+
+## Model Training
+* load data
+```python
+df_training = pd.read_csv(args.training)
+```
+* Use ARIMA to train model
+```python
+arima = auto_arima(df_training[c0], start_p=1, start_q=1,max_p=3, max_q=3, m=12,start_P=0, seasonal=True, d=1, D=1, trace=True, error_action='ignore',suppress_warnings=True)
+```
+* Predict
+```python
+result = arima.predict(n_periods = 20)
+```
+* Write csv
+```python
+df_res = pd.DataFrame(res)
+df_res.to_csv(args.output, index = 0)
+```
+
+
+
--- trader.py
+++ trader.py
@@ -1,5 +1,4 @@
-# USAGE: 
-#   python trader.py --training training.csv --testing testing.csv --output output.csv
+# USAGE: python trader.py --training training.csv --testing testing.csv --output output.csv
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -30,14 +29,58 @@ if __name__ == '__main__':
 
     df_training = pd.read_csv(args.training)
     c0 = df_training.columns[0]
-    print(df_training[c0])
-    # arima = auto_arima(df_training["operating_reserve(MW)"], seasonal=True)
 
     arima = auto_arima(df_training[c0], start_p=1, start_q=1,max_p=3, max_q=3, m=12,start_P=0, seasonal=True, d=1, D=1, trace=True, error_action='ignore',suppress_warnings=True)
-    result = arima.predict(n_periods = 20)
+    result = arima.predict(n_periods = 20) # Predict the stock price of the following 20 days 
     result = result.astype('float32')
-    print(result)
 
-    df_result = pd.DataFrame(result)
-    df_result.to_csv(args.output, index=0)
+    # print(result) 
 
+
+    prev = 0
+    stock = [] # rise or fall
+    isfirst = True
+    for next in result:
+        if(isfirst):
+            isfirst = False
+            prev = next
+            continue
+
+        if(prev > next):
+            stock.append(0)          
+        else:
+            stock.append(1) 
+        prev = next
+
+    now = 0 # have stock or not
+    res = []
+    isfirst = True
+    i = 0
+    for tomo in stock:
+        i = i + 1
+        if(i == 20): 
+            break
+
+        if(isfirst):
+            isfirst = False
+            continue
+
+        if(now == 0):       # no stock
+            if(tomo):   # rise
+                now = 1
+                res.append(1)       # buy
+            else:               # fall
+                now = 0
+                res.append(0)       # nope
+
+        else:               # have stock
+            if(tomo):   # rise   
+                now = 1
+                res.append(0)    # hold
+            else:               # fall
+                now = 0
+                res.append(-1)     # sell 
+        
+
+    df_res = pd.DataFrame(res)
+    df_res.to_csv(args.output, index = 0)
