de26980c15ca9c0aa9bb714c18d7bd866a59318b
Jonathan881005 <a1813203@gmail.com>
2021-04-10 20:03:02 +0800

    Update trader.py

--- trader.py
+++ trader.py
@@ -27,12 +27,12 @@ if __name__ == '__main__':
     # You can modify it at will.
 
 
-    df_training = pd.read_csv(args.training)
+    df_training = pd.read_csv(args.training, head = None)
     c0 = df_training.columns[0]
 
-    df_testing = pd.read_csv(args.testing)
+    df_testing = pd.read_csv(args.testing, head = None)
     period = df_testing.shape[0] # number of columns
-    period = period + 1
+    period = period
     # print(period)
 
     arima = auto_arima(df_training[c0], start_p=1, start_q=1,max_p=3, max_q=3, m=12,start_P=0, seasonal=True, d=1, D=1, trace=True, error_action='ignore',suppress_warnings=True)
@@ -45,18 +45,18 @@ if __name__ == '__main__':
     prev = 0
     stock = [] # rise or fall
     isfirst = True
-    for next in result:
+    for nxt in result:
         if(isfirst):
             isfirst = False
-            prev = next
+            prev = nxt
             continue
 
-        if(prev > next):
+        if(prev > nxt):
             stock.append(0)          
         else:
             stock.append(1) 
 
-        prev = next
+        prev = nxt
 
     print(stock)
 
@@ -64,14 +64,22 @@ if __name__ == '__main__':
     res = []
     isfirst = True
     for tomo in stock:
+        if(now == -1):       # short 
+            if(tomo):   # rise
+                now = 0
+                res.append(1)
+            else:       # fall
+                now = -1
+                res.append(0)
+
 
         if(now == 0):       # no stock
             if(tomo):   # rise
                 now = 1
                 res.append(1)       # buy
             else:               # fall
-                now = 0
-                res.append(0)       # nope
+                now = -1
+                res.append(-1)       # nope
 
         else:               # have stock
             if(tomo):   # rise   
